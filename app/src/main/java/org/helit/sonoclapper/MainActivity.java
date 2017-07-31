1f6f194e8ea3389242804b2857ac76ef23b1d042
Fantomas4 <stef1997s@gmail.com>
2017-07-31 20:57:54 +0300

    Added increment/decrement function for -/+ buttons, worked on finishing functional counters for scene/view/take.

--- app/src/main/java/org/helit/sonoclapper/MainActivity.java
+++ app/src/main/java/org/helit/sonoclapper/MainActivity.java
@@ -6,7 +6,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    private int scene_count = 0;
+    private int view_count = 0;
+    private int take_count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,12 +19,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void grapseStart(View b) {
+    public void num_increment(View b) {
+
+        switch(b.getId())
+        {
+            case R.id.button2:
+                scene_count +=1;
+                break;
+            case R.id.button4:
+                view_count += 1;
+                break;
+            case R.id.button6:
+                take_count += 1;
+                break;
+            default:
+                throw new RuntimeException("Unknown button ID");
+        }
         //b.setEnabled(false);
 
-
-        Toast bread = Toast.makeText(getApplicationContext(), "Congrats! You pressed the button!!!", Toast.LENGTH_SHORT);
+        Toast bread = Toast.makeText(getApplicationContext(), "Increment!", Toast.LENGTH_SHORT);
         bread.show();
+    }
+
+    public void num_decrement(View b) {
+
+        switch(b.getId())
+        {
+            case R.id.button1:
+                scene_count -=1;
+                break;
+            case R.id.button3:
+                view_count -= 1;
+                break;
+            case R.id.button5:
+                take_count -= 1;
+                break;
+            default:
+                throw new RuntimeException("Unknown button ID");
+        }
+
+        Toast bread = Toast.makeText(getApplicationContext(), "Decrement!", Toast.LENGTH_SHORT);
+        bread.show();
+
+
     }
 }
 
