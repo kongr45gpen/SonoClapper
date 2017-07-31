5bc52b5c3579a8e641c2c1b88858dc9991c61f1a
Fantomas4 <stef1997s@gmail.com>
2017-08-01 02:55:45 +0300

    Worked on increment/decrement buttons for Scene/View/Take, and Scene/View counters are now functional. Bug that keeps Take counter stuck at 0 even when clicking on thw -/+ buttons needs fix!

--- app/src/main/java/org/helit/sonoclapper/MainActivity.java
+++ app/src/main/java/org/helit/sonoclapper/MainActivity.java
@@ -4,19 +4,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private int scene_count = 0;
-    private int view_count = 0;
-    private int take_count = 0;
+    int scene_count = 0;
+    int view_count = 0;
+    int take_count = 0;
+    TextView sceneText;
+    TextView viewText;
+    TextView takeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+        sceneText = (TextView) findViewById(R.id.scene_edit_text);
+        viewText = (TextView) findViewById(R.id.view_edit_text);
+        takeText = (TextView) findViewById(R.id.take_edit_text);
+
+
     }
 
     public void num_increment(View b) {
@@ -24,21 +39,25 @@ public class MainActivity extends AppCompatActivity {
         switch(b.getId())
         {
             case R.id.button2:
-                scene_count +=1;
+                scene_count += 1;
+                sceneText.setText(Integer.toString(scene_count));
                 break;
             case R.id.button4:
                 view_count += 1;
+                viewText.setText(Integer.toString(view_count));
                 break;
             case R.id.button6:
                 take_count += 1;
+                takeText.setText(Integer.toString(take_count));
                 break;
             default:
                 throw new RuntimeException("Unknown button ID");
         }
-        //b.setEnabled(false);
 
-        Toast bread = Toast.makeText(getApplicationContext(), "Increment!", Toast.LENGTH_SHORT);
-        bread.show();
+
+
+        Toast notify = Toast.makeText(getApplicationContext(), "Increment!", Toast.LENGTH_SHORT);
+        notify.show();
     }
 
     public void num_decrement(View b) {
@@ -47,19 +66,24 @@ public class MainActivity extends AppCompatActivity {
         {
             case R.id.button1:
                 scene_count -=1;
+                sceneText.setText(Integer.toString(scene_count));
                 break;
             case R.id.button3:
                 view_count -= 1;
+                viewText.setText(Integer.toString(view_count));
                 break;
             case R.id.button5:
                 take_count -= 1;
+                takeText.setText(Integer.toString(take_count));
                 break;
             default:
                 throw new RuntimeException("Unknown button ID");
         }
 
-        Toast bread = Toast.makeText(getApplicationContext(), "Decrement!", Toast.LENGTH_SHORT);
-        bread.show();
+
+
+        Toast notify = Toast.makeText(getApplicationContext(), "Decrement!", Toast.LENGTH_SHORT);
+        notify.show();
 
 
     }
